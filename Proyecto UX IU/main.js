9a5986d9fd30ca61db48609ba875e9ee0bb642ee
Natasaur <n.garcia.s787@edu.utc.mx>
2025-03-03 10:10:20 -0600

    Clase 03/03/2025

---
+++ Proyecto UX IU/main.js
@@ -0,0 +1,17 @@
+"use strict"; //modo estricto para elegir las variables
+let nombre = "Ana López"; //variable
+const pi = 3.1416; //constante
+var ciudad = "CDMX";  //variable
+
+console.log(nombre, pi, ciudad);
+
+let texto = "los gatos son los mejores";
+let numero = 12;
+let numero2 = 1;
+let bool = true;
+
+console.log(typeof texto, typeof numero, typeof bool);
+
+let nombre2 = prompt("¿Cómo te llamas?");
+
+alert("Bienvenida(o)  " + nombre2)
