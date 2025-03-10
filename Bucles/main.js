c4e4ffb31a4fb442d98e089df57c852ca795430e
Natasaur <n.garcia.s787@edu.utc.mx>
2025-03-10 08:57:21 -0600

    Actividades, API con datos en tiempo real y tabla del 1 en la consola de JS

--- Bucles/main.js
+++ Bucles/main.js
@@ -1,18 +1,22 @@
 // bucles 
 // while rectifica el código
-for (let i = 0; i <= 10; i++) {
+/*for (let i = 0; i <= 10; i++) {
     console.log("con for " + i);
 }
-/*
+console.log("HOLA MUNDO!!!");
+
 let contador = 1
 while (contador <= 10) {
     console.log("con while " + contador);
     contador++;
 }
-*/
+
 let contador = 10
 do {
     console.log("con while se va a imprimir una vez este bien o mal " + contador);
     contador++;
 }
 while (contador <= 10);
+*/
+
+
