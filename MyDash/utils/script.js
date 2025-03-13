f965166238127031db2c75fe6d56b70876979f24
Natasaur <n.garcia.s787@edu.utc.mx>
2025-03-13 07:57:20 -0600

    Clase 13/03/2025 My Dashboard

---
+++ MyDash/utils/script.js
@@ -0,0 +1,12 @@
+const llave = ''; //https://home.openweathermap.org/subscriptions/unauth_subscribe/onecall_30/base
+
+let input = document.getElementById('input');
+let button = document.getElementById('button');
+let ciudad;
+let api;
+let line;
+let pie;
+
+button.addEventListener("click", (event)=>{
+   
+});
